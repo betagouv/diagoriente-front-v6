1f92c75495f5b7cf6bacbd944cf5c0b3d63668ab
mlorthios <mathislts62@gmail.com>
2021-06-21 16:38:29 +0200

    fix activites done

--- src/containers/WIPExperienceContainer/containers/WIPAddActivityDone.tsx
+++ src/containers/WIPExperienceContainer/containers/WIPAddActivityDone.tsx
@@ -1,26 +1,30 @@
-import React, { FunctionComponent } from 'react';
-import Illustration from 'assets/svg/illu_01.svg';
+import React, { FunctionComponent } from "react";
+import Illustration from "assets/svg/illu_01.svg";
+import ParcoursLayout from "../ParcoursLayout";
 
 const WipAddActivityDone: FunctionComponent = () => (
-  <div className="bg-lena-blue-darkest min-h-screen flex flex-col flex-1">
-    <div className="text-white flex flex-col flex-1 items-center justify-center">
-      <div className="container flex flex-col items-center justify-center space-y-8">
-        <div className="text-2xl font-bold">Merci !</div>
-        <div className="w-full flex justify-center">
-          <img src={Illustration} style={{ width: '60%', height: 'auto' }} alt="Illustration" />
-        </div>
-        <div className="text-center">
-          Maintenant <strong>sélectionnez les compétences</strong> que vous mettez en oeuvre dans le cadre de ces
-          activités.
-        </div>
-        <div className="fixed bottom-0 left-0 right-0">
-          <button className="focus:ring-0 focus:outline-none w-full bg-lena-blue text-white py-3 text-center font-bold text-lg">
-            Sélectionner les compétences
-          </button>
+  <ParcoursLayout>
+    <div className="bg-lena-blue-darkest min-h-screen flex flex-col flex-1">
+      <div className="text-white flex flex-col flex-1 items-center justify-center">
+        <div className="container flex flex-col items-center justify-center space-y-8">
+          <div className="text-2xl font-bold">Merci !</div>
+          <div className="w-full flex justify-center">
+            <img src={Illustration} alt="Illustration" />
+          </div>
+          <div className="text-center">
+            Maintenant <strong>sélectionnez les compétences</strong> que vous mettez en oeuvre dans le cadre de ces
+            activités.
+          </div>
+          <div className="fixed bottom-0 left-0 right-0 md:relative">
+            <button
+              className="md:px-14 md:rounded-md focus:ring-0 focus:outline-none w-full bg-lena-blue text-white py-3 text-center font-bold text-lg">
+              Sélectionner les compétences
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </ParcoursLayout>
 );
 
 export default WipAddActivityDone;
