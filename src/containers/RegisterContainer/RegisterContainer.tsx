2a69710cfbab9be2f9e2a827fa3d1c6d48d0daf5
mlorthios <izikore@gmail.com>
2021-06-22 12:00:24 +0200

    fix mobile

--- src/containers/RegisterContainer/RegisterContainer.tsx
+++ src/containers/RegisterContainer/RegisterContainer.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
+import React from "react";
 import HeroRegister from "assets/illu/hero-register.png";
 import BgRegister from "assets/images/bg/bg-register.png";
-import RegisterForm from './RegisterForm';
+import RegisterForm from "./RegisterForm";
 import StaticHeader from "../../layouts/StaticHeader";
 
-const RegisterContainer = () => (
-  <div>
-    <StaticHeader />
-    <div className="md:h-screen w-full flex-1 flex flex-row">
-      <div className="flex-1 px-16 pt-16 bg-lena-lightgray z-10 flex flex-col gap-y-16">
-        <div className="">
-          <h2 className="font-bold text-lena-blue-dark text-3xl mb-2">Inscription</h2>
-          <span className="text-lena-turquoise-dark font-bold">J'ai déjà un compte</span>
+const RegisterContainer = () => {
+  return (
+    <div>
+      <StaticHeader />
+      <div className="md:h-screen w-full flex-1 flex flex-row">
+        <div className="flex-1 md:px-16 px-4 pt-16 bg-lena-lightgray z-10 flex flex-col space-y-16">
+          <div className="">
+            <h2 className="font-bold text-lena-blue-dark text-3xl mb-2">Inscription</h2>
+            <span className="text-lena-turquoise-dark font-bold">J'ai déjà un compte</span>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full mb-10 md:mb-0">
+            <RegisterForm />
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-y-8 w-full mb-10 md:mb-0">
-          <RegisterForm />
-          <button className="bg-lena-pink w-full text-white font-bold md:w-72 py-3 rounded-md">Finaliser mon inscription</button>
-        </div>
-      </div>
-      <div className="bg-lena-blue h-full hidden xl:block" style={{ width: 568, background: `url(${BgRegister}) no-repeat center`, backgroundSize: "cover" }}>
-        <div className="invisible xl:visible absolute top-0 right-0">
-          <img placeholder="blurred" src={HeroRegister} alt="Illustration" />
+        <div className="bg-lena-blue h-full hidden xl:block" style={{
+          width: 568,
+          background: `url(${BgRegister}) no-repeat center`,
+          backgroundSize: "cover"
+        }}>
+          <div className="invisible xl:visible absolute top-0 right-0">
+            <img placeholder="blurred" src={HeroRegister} alt="Illustration" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RegisterContainer;
--- src/containers/RegisterContainer/RegisterForm.tsx
+++ src/containers/RegisterContainer/RegisterForm.tsx
@@ -3,74 +3,92 @@ import InputComponent from "components/Register/Input";
 import Checkbox from "components/Register/Checkbox";
 import { FormControl, FormLabel } from "components/Register/FormController";
 import FormComment from "components/Register/FormComment";
+import { useFormik } from "formik";
 
 const RegisterForm: FunctionComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: ""
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
   return (
-    <div className="flex flex-col gap-y-4 w-full">
-      <div>
-        <FormControl>
-          <FormLabel>Prénom *</FormLabel>
-          <InputComponent type="text" placeholder="ex : Léna" />
-        </FormControl>
-      </div>
-      <div>
-        <FormControl>
-          <FormLabel>Nom de famille *</FormLabel>
-          <InputComponent type="text" placeholder="ex : MAZILU" />
-        </FormControl>
-      </div>
-      <div className="mt-4">
-        <FormControl>
-          <FormLabel>Email *</FormLabel>
-          <InputComponent checked={true} type="email" isInvalid={false} placeholder="test@test.dev" />
-        </FormControl>
-      </div>
-      <div>
-        <FormControl>
-          <FormLabel>Mot de passe *</FormLabel>
-          <InputComponent checked={true} type="password" isInvalid={true} />
-        </FormControl>
-      </div>
-      <div>
-        <FormControl>
-          <FormLabel />
-          <FormComment>
-            <div>Votre mot de passe doit comporter 12 caractères minimum, dont :</div>
-            <ul className="list-disc ml-4 mt-1">
-              <li className="font-bold">1 majuscule</li>
-              <li className="font-bold">1 minuscule</li>
-              <li className="font-bold">1 chiffre</li>
-              <li className="font-bold">1 caractère spécial</li>
-            </ul>
-          </FormComment>
-        </FormControl>
-      </div>
-      <div className="mt-4">
-        <FormControl>
-          <FormLabel>Ville de résidence</FormLabel>
-          <InputComponent />
-        </FormControl>
-      </div>
-      <div>
-        <FormControl>
-          <FormLabel>Code groupe</FormLabel>
-          <InputComponent />
-        </FormControl>
-      </div>
-      <div className="mt-4 flex justify-center">
-        <Checkbox
-          required={true}
-          label={
-            <>
-              J'accepte les{" "}
-              <span className="text-lena-blue-dark font-bold">
+    <div className="flex flex-col w-full">
+      <form className="flex flex-col w-full" onSubmit={formik.handleSubmit}>
+        <div className="mb-4">
+          <FormControl>
+            <FormLabel htmlFor="firstName">Prénom *</FormLabel>
+            <InputComponent id="firstName" name="firstName" type="text" placeholder="ex : Léna" />
+          </FormControl>
+        </div>
+        <div className="mb-7">
+          <FormControl>
+            <FormLabel>Nom de famille *</FormLabel>
+            <InputComponent type="text" placeholder="ex : MAZILU" />
+          </FormControl>
+        </div>
+        <div className="mb-3">
+          <FormControl>
+            <FormLabel>Email *</FormLabel>
+            <InputComponent checked={true} type="email" isInvalid={false} placeholder="test@test.dev" />
+          </FormControl>
+        </div>
+        <div className="mb-3">
+          <FormControl>
+            <FormLabel>Mot de passe *</FormLabel>
+            <InputComponent checked={true} type="password" isInvalid={true} />
+          </FormControl>
+        </div>
+        <div className="mb-3">
+          <FormControl>
+            <FormLabel />
+            <FormComment>
+              <div>Votre mot de passe doit comporter 12 caractères minimum, dont :</div>
+              <ul className="list-disc ml-4 mt-1">
+                <li className="font-bold">1 majuscule</li>
+                <li className="font-bold">1 minuscule</li>
+                <li className="font-bold">1 chiffre</li>
+                <li className="font-bold">1 caractère spécial</li>
+              </ul>
+            </FormComment>
+          </FormControl>
+        </div>
+        <div className="mb-3">
+          <FormControl>
+            <FormLabel>Ville de résidence</FormLabel>
+            <InputComponent />
+          </FormControl>
+        </div>
+        <div className="mb-3">
+          <FormControl>
+            <FormLabel>Code groupe</FormLabel>
+            <InputComponent />
+          </FormControl>
+        </div>
+        <div className="mt-4 flex justify-center">
+          <Checkbox
+            required={true}
+            label={
+              <>
+                J'accepte les{" "}
+                <span className="text-lena-blue-dark font-bold">
                 conditions d'utilisation
               </span>{" "}
-              de Diagoriente
-            </>
-          }
-        />
-      </div>
+                de Diagoriente
+              </>
+            }
+          />
+        </div>
+        <div className="text-center mt-7 pb-10">
+          <button className="bg-lena-pink w-full text-white font-bold md:w-72 py-3 rounded-md">Finaliser mon
+            inscription
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
