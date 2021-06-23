f9cd4de6259c4fa02398b305582feffd98260084
moezbenjemaa <moezbj.93@gmail.com>
2021-06-23 13:14:27 +0100

    add login logic

--- src/containers/LoginContainer/Login.tsx
+++ src/containers/LoginContainer/Login.tsx
@@ -1,50 +1,60 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { validateEmail, validatePassword } from 'common/utils/validation';
+import InputComponent from 'components/Register/Input';
+import { FormControl, FormLabel } from 'components/Register/FormController';
+import FormComment from 'components/Register/FormComment';
+import useLogin from 'common/container/auth/useLogin';
 import AppLayout from '../../layouts/AppLayout';
 
 const LoginContainer = () => {
-  const initialValues = { email: '', password: '' };
-  const validateForm = (values: { email: string; password: string }) => {
-    const errors = {} as { email: string; password: string };
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (validateEmail(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-    if (!values.password) {
-      errors.password = 'Required';
-    } else {
-      errors.password = validatePassword(values.password);
-    }
-    return errors;
-  };
+  const { user, formik, errorForm, loginState } = useLogin();
   return (
     <AppLayout>
       <span>Login</span>
       <div>
-        <Formik
-          initialValues={initialValues}
-          validate={(values) => validateForm(values)}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field type="email" name="email" placeholder="example@domain.com" />
-              <ErrorMessage name="email" component="div" />
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-2">
+            <FormControl>
+              <FormLabel htmlFor="email">Email *</FormLabel>
+              <InputComponent
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                id="email"
+                name="email"
+                checked={true}
+                type="email"
+                isInvalid={!!formik.errors.email}
+                placeholder="test@test.dev"
+              />
+            </FormControl>
+          </div>
+          {formik.touched.email && formik.errors.email ? (
+            <div className="mb-2">
+              <FormControl>
+                <FormLabel />
+                <FormComment>
+                  <div className="text-lena-pink">{formik.errors.email}</div>
+                </FormComment>
+              </FormControl>
+            </div>
+          ) : null}
+          <div className="mb-2">
+            <FormControl>
+              <FormLabel htmlFor="password">Mot de passe *</FormLabel>
+              <InputComponent
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                id="password"
+                name="password"
+                checked={true}
+                type="password"
+                isInvalid={!!formik.errors.password}
+              />
+            </FormControl>
+          </div>
+          <div className="text-center mt-7 pb-10">
+            <button className="bg-lena-pink w-full text-white font-bold md:w-72 py-3 rounded-md">connexion</button>
+          </div>
+        </form>
       </div>
     </AppLayout>
   );
