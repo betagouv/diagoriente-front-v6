bd6acb28e3cac0beb270ba565d517e9676dc9fe9
mlorthios <mathislts62@gmail.com>
2021-06-24 10:47:10 +0200

    add export button to skill card

--- src/containers/SkillCard/SkillCardExport.tsx
+++ src/containers/SkillCard/SkillCardExport.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import Checkbox from 'components/Register/Checkbox';
 import Star from 'components/design-system/Star';
-import MedailleSvg from "assets/svg/medaille.svg";
-import CloseSvg from "assets/svg/cross2.svg";
+import MedailleSvg from 'assets/svg/medaille.svg';
+import CloseSvg from 'assets/svg/cross2.svg';
+import ModalExportSvg from 'assets/svg/export_red.svg';
 
 type SkillProps = {
   star: number;
@@ -48,12 +49,12 @@ type ExperienceProps = {
 
 const Experience: React.FC<ExperienceProps> = ({ checked, title, date, exp, recommended }) => {
   return (
-    <div className={
-      clsx(
-        "flex mb-7 bg-white p-5 rounded-md",
-        !recommended?.checked && !checked ? "opacity-50" : !checked && !recommended?.checked && "opacity-50"
-      )
-    }>
+    <div
+      className={clsx(
+        'flex mb-7 bg-white p-5 rounded-md',
+        !recommended?.checked && !checked ? 'opacity-50' : !checked && !recommended?.checked && 'opacity-50',
+      )}
+    >
       <h3 className="text-lena-blue-dark font-bold mr-2">
         <Checkbox checked={recommended?.checked ? true : checked} label="" />
       </h3>
@@ -66,25 +67,19 @@ const Experience: React.FC<ExperienceProps> = ({ checked, title, date, exp, reco
           ))}
         </ul>
         {typeof recommended !== 'undefined' && (
-          <div className={
-            clsx(
-              "flex mt-5",
-              !recommended?.checked && "opacity-50"
-            )
-          }>
-
+          <div className={clsx('flex mt-5', !recommended?.checked && 'opacity-50')}>
             <Checkbox checked={recommended.checked} label="" />
             <div className="bg-lena-yellow-light p-5 rounded-md">
               <div className="flex items-center">
                 <img src={MedailleSvg} alt="Medaille Svg" />
                 <span className="bg-lena-yellow inline-block ml-4 px-3 text-sm py-1 rounded-md bg-opacity-50 font-bold">
-                EXPÉRIENCE RECOMMANDÉE
-              </span>
+                  EXPÉRIENCE RECOMMANDÉE
+                </span>
               </div>
               <div className="mt-3">
-              <span className="block" style={{ color: '#424242' }}>
-                "{recommended.description}"
-              </span>
+                <span className="block" style={{ color: '#424242' }}>
+                  "{recommended.description}"
+                </span>
                 <span className="block italic mt-2">{recommended.signature}</span>
               </div>
             </div>
@@ -105,11 +100,16 @@ const SkillCardExport = ({ onClose }: SkillCardExportProps) => {
     document.body.style.overflow = 'auto';
   }, [document]);
   return (
-    <div className="mb-10 overflow-auto">
+    <div className="mb-32 overflow-auto">
       <nav className="mb-6" style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
         <div className="flex justify-between container py-6">
           <span className="font-bold">Sélectionnez ce qui sera visible sur votre carte de compétences :</span>
-          <button onClick={() => onClose.call(null)} className="text-lena-pink-dark flex items-center focus:ring-0 focus:outline-none">Fermer <img className="ml-3" src={CloseSvg} alt="Cross Icon" /></button>
+          <button
+            onClick={() => onClose.call(null)}
+            className="text-lena-pink-dark flex items-center focus:ring-0 focus:outline-none"
+          >
+            Fermer <img className="ml-3" src={CloseSvg} alt="Cross Icon" />
+          </button>
         </div>
       </nav>
       <div className="container">
@@ -129,8 +129,17 @@ const SkillCardExport = ({ onClose }: SkillCardExportProps) => {
                 <Checkbox label="" />
                 <h5 className="ml-2 text-lena-blue-dark uppercase font-bold text-lg">Mes compétences transversales</h5>
               </div>
-              <Skill star={4} title="Agir collectivement" description="Je fais des propositions au groupe, j'écoute et prends en compte l'avis de chacun, exprime des avis contraires." bottom={true} />
-              <Skill star={4} title="Organiser son activité" description="Je m’organise en fonction d'imprévus, je prends quelques initiatives." />
+              <Skill
+                star={4}
+                title="Agir collectivement"
+                description="Je fais des propositions au groupe, j'écoute et prends en compte l'avis de chacun, exprime des avis contraires."
+                bottom={true}
+              />
+              <Skill
+                star={4}
+                title="Organiser son activité"
+                description="Je m’organise en fonction d'imprévus, je prends quelques initiatives."
+              />
             </div>
             <div
               className={clsx('mt-3 bg-lena-turquoise-light p-6 rounded-lg', false && 'opacity-50')}
@@ -140,8 +149,17 @@ const SkillCardExport = ({ onClose }: SkillCardExportProps) => {
                 <Checkbox label="" />
                 <h5 className="ml-2 text-lena-blue-dark uppercase font-bold text-lg">Mes compétences d'engagement</h5>
               </div>
-              <Skill star={4} title="Prendre en compte les codes sociaux dans l’activité" description="Je fais des propositions au groupe, j'écoute et prends en compte l'avis de chacun, exprime des avis contraires." bottom={true} />
-              <Skill star={4} title="Prendre en compte les règlements dans le cadre de sa mission" description="Je m’organise en fonction d'imprévus, je prends quelques initiatives." />
+              <Skill
+                star={4}
+                title="Prendre en compte les codes sociaux dans l’activité"
+                description="Je fais des propositions au groupe, j'écoute et prends en compte l'avis de chacun, exprime des avis contraires."
+                bottom={true}
+              />
+              <Skill
+                star={4}
+                title="Prendre en compte les règlements dans le cadre de sa mission"
+                description="Je m’organise en fonction d'imprévus, je prends quelques initiatives."
+              />
             </div>
           </div>
           <div>
@@ -219,6 +237,12 @@ const SkillCardExport = ({ onClose }: SkillCardExportProps) => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 py-7 bg-lena-pink-dark flex justify-center">
+        <button className="text-lena-pink-dark bg-white items-center flex px-14 py-2 rounded-md">
+          <img className="mr-2" alt="Export Icon" src={ModalExportSvg} />
+          Exporter
+        </button>
       </div>
     </div>
   );
