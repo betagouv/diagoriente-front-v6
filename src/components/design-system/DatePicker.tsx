1391c43d375bfef7ce1d41b3bffd9ff6d93b7761
moezbenjemaa <moezbj.93@gmail.com>
2021-07-08 14:11:25 +0100

    add date component

--- src/components/design-system/DatePicker.tsx
+++ src/components/design-system/DatePicker.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+/* eslint-disable react/require-default-props */
+import React, { useState } from 'react';
+import { ReactComponent as ArrowDownSvg } from 'assets/svg/arrow_down.svg';
+import InputComponent from 'components/Register/Input';
+import Checkbox from 'components/Register/Checkbox';
+import { mois } from 'utils/staticMonths';
+import classNames from 'common/utils/classNames';
 
 interface DatePickerType {
   title: string;
@@ -6,17 +12,72 @@ interface DatePickerType {
   year: string;
   setMonth: (s: string) => void;
   setYear: (y: string) => void;
+  subTitle?: string;
+  hasCheckBox?: boolean;
 }
 
-const DatePicker = ({ title, month, year, setMonth, setYear }: DatePickerType) => {
+const DatePicker = ({ title, subTitle, month, year, hasCheckBox, setMonth, setYear }: DatePickerType) => {
+  const [open, setOpen] = useState(false);
+  const onHandleChange = (e: any) => {
+    const { value } = e.target;
+    setYear(value);
+  };
   return (
-    <div>
-      <div>{title}</div>
-      <div className="flex flex-row items-center ">
-        <p>Mois</p>{' '}
-        <div className="border rounded-md focus:ring-0 focus:outline-none pt-3 pb-3 ml-5 px-6 border-lena-turquoise-dark">
-          {month}
+    <div className="p-14">
+      <div>
+        <strong>{title}</strong> {subTitle && <span>{subTitle}</span>}
+      </div>
+      <div>
+        <div className="flex flex-row items-center mt-5">
+          <div className="flex flex-row items-center relative ">
+            <strong className="text-lena-blue-dark">Mois</strong>{' '}
+            <div
+              style={{ borderColor: '#e1e7f7' }}
+              className="flex w-40 items-center justify-between border rounded-md focus:ring-0 focus:outline-none pt-2 pb-2 ml-5 px-6 shadow text-lena-blue-dark"
+              onClick={() => setOpen(!open)}
+            >
+              {mois[Number(month) - 1]?.title || 'janvier'}
+              <div className=" ml-6">
+                <ArrowDownSvg />
+              </div>
+            </div>
+            {open && (
+              <div className="w-80 h-50 border radio-4 absolute top-14 left-14 bg-white z-10 shadow">
+                <div className="flex flex-wrap w-full">
+                  {mois.map((m) => (
+                    <div
+                      onClick={() => {
+                        setMonth(m.value);
+                        setOpen(false);
+                      }}
+                      className="w-2/6 p-3 align-center cursor-pointer "
+                    >
+                      <span className={classNames(m.value === month ? 'text-lena-blue-dark font-bold' : '')}>
+                        {m.title}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-row items-center ml-20">
+            <strong className="mr-5 text-lena-blue-dark">Année</strong>{' '}
+            <InputComponent
+              value={year}
+              onChange={onHandleChange}
+              id="year"
+              name="firstName"
+              type="text"
+              placeholder="AAAA"
+            />
+          </div>
         </div>
+        {hasCheckBox && (
+          <div className="mt-10">
+            <Checkbox required={false} label={<>Cette expérience est toujours en cours</>} />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/containers/ExperienceContainer/CommonContainers/DateContainer.tsx
+++ src/containers/ExperienceContainer/CommonContainers/DateContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useAddSkill } from 'common/requests/skills';
 import { useHistory } from 'react-router-dom';
+import moment from 'moment';
 import { NewExperienceContext, EParcoursStep } from 'contexts/NewExperienceContext';
 import { ReactComponent as ArrowLeftSvg } from 'assets/images/svg/picto/arrow-left.svg';
 import DatePicker from 'components/design-system/DatePicker';
@@ -11,16 +12,35 @@ const DateContainer = () => {
   const { theme, competencesValues, activities, setCompetencesValues, setStep, setCompetences } =
     useContext(NewExperienceContext);
   const [addSkillCall, addSkillState] = useAddSkill();
-  const [month, setMonth] = useState('Janvier');
-  const [year, setYear] = useState('');
+  const [monthStart, setMonthStart] = useState('Janvier');
+  const [yearStart, setYearStart] = useState('');
+  const [monthEnd, setMonthEnd] = useState('Janvier');
+  const [yearEnd, setYearEnd] = useState('');
+  const [currentTime, setCurrentTime] = useState(false);
 
   const onAddSkill = () => {
     if (theme?.id && activities.length && competencesValues.length) {
-      const dataToSend = {
+      const sD = moment(`${monthStart}-${yearStart}`).format('MMMM-AAAA');
+      console.log('sd', sD);
+      const dataToSend: {
+        theme: string;
+        activities: string[];
+        competences: {
+          competence: string;
+          value: number;
+        }[];
+        startDate: string;
+        endDate?: string;
+      } = {
         theme: theme?.id,
         activities: activities.map((act) => act.id),
         competences: competencesValues.map((cmp) => ({ competence: cmp.id, value: cmp.value })),
+        startDate: sD,
       };
+      if (monthEnd && yearEnd) {
+        const sE = moment(`${monthEnd}-${yearEnd}`).format('MMMM-AAAA');
+        dataToSend.endDate = sE;
+      }
       addSkillCall({ variables: dataToSend });
     }
   };
@@ -31,7 +51,10 @@ const DateContainer = () => {
   }, []);
   return (
     <ParcoursLayout>
-      <button onClick={() => history.push('/')} className="flex items-center mb-5 focus:ring-0 focus:outline-none">
+      <button
+        onClick={() => setStep(EParcoursStep.COMPETENCES)}
+        className="flex items-center mt-5 ml-5 focus:ring-0 focus:outline-none"
+      >
         <ArrowLeftSvg />
         <span className="text-sm mt-1 ml-3 text-lena-blue-dark">Retour</span>
       </button>
@@ -41,9 +64,30 @@ const DateContainer = () => {
             Pour finir, à quelles dates s’est déroulée cette expérience de <strong> boulangerie </strong> ? (facultatif){' '}
           </div>
         </div>
+        <DatePicker
+          title="Date de début"
+          month={monthStart}
+          year={yearStart}
+          setMonth={setMonthStart}
+          setYear={setYearStart}
+        />
+        <DatePicker
+          title="Date de fin"
+          subTitle="(facultatif)"
+          month={monthEnd}
+          year={yearEnd}
+          setMonth={setMonthEnd}
+          setYear={setYearEnd}
+          hasCheckBox
+        />
       </div>
-      <div className="space-y-8 container py-8 md:p-14 ">
-        <DatePicker title="Date début" month={month} year={year} setMonth={setMonth} setYear={setYear} />
+      <div className="flex flex-col items-center justify-start space-y-8 container py-8 md:p-14 relative">
+        <button
+          onClick={onAddSkill}
+          className="focus:ring-0 focus:outline-none w-full bg-lena-blue text-white py-3 text-center font-bold text-lg md:w-72 md:rounded-lg"
+        >
+          Valider
+        </button>
       </div>
     </ParcoursLayout>
   );
