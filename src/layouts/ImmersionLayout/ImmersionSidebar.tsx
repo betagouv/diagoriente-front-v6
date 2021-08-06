4adade469db53e1e68e26aeb307c8db4d3114638
Mérill <merill.teterel@id6tm.org>
2021-08-06 09:39:03 +0200

    Fix sidebar widths

--- src/layouts/ImmersionLayout/ImmersionSidebar.tsx
+++ src/layouts/ImmersionLayout/ImmersionSidebar.tsx
@@ -4,7 +4,7 @@ import ImmersionSearchForm from '../../containers/ImmersionContainer/components/
 
 const ImmersionSidebar: FunctionComponent<{ showSearch: boolean }> = ({ showSearch }) => {
   return (
-    <div className="w-96 bg-lena-lightgray flex flex-col overflow-auto filter drop-shadow-sm z-10">
+    <div className="w-80 bg-lena-lightgray flex flex-col overflow-auto filter drop-shadow-sm z-10">
       <div className="flex flex-col justify-between flex-grow">
         <div className="flex flex-col space-y-8 p-8">
           <div className="flex flex-col items-center justify-center space-y-4">
--- src/layouts/JobsLayout/JobsSidebar.tsx
+++ src/layouts/JobsLayout/JobsSidebar.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as TopJobsIcon } from 'assets/svg/picto_top_metiers.svg'
 
 const JobsSidebar: FunctionComponent = () => {
   return (
-    <div className="w-96 bg-lena-lightgray flex flex-col top-0 left-0 relative filter drop-shadow-sm z-10">
+    <div className="w-80 bg-lena-lightgray flex flex-col top-0 left-0 relative filter drop-shadow-sm z-10">
       <div className="flex flex-col justify-between flex-grow">
         <div className="flex flex-col space-y-8 p-8">
           <div className="flex flex-col items-center justify-center space-y-4">
