9ad81b7032ede28b51ab1bc4955be3f02274432a
Mérill <merill.teterel@id6tm.org>
2021-08-09 16:07:17 +0200

    TopJobs redesign

--- src/components/design-system/Pill.tsx
+++ src/components/design-system/Pill.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import classNames from 'common/utils/classNames';
 
-const Pill: FunctionComponent<{ size?: 'sm' }> = ({ size, children }) => {
+const Pill: FunctionComponent<{ extraClassName?: string; size?: 'sm' }> = ({ extraClassName, size, children }) => {
   return (
     <div
       className={classNames(
         'inline-block bg-lena-blue-lightest text-lena-blue-dark rounded-full px-3 py-1 text-center',
         size === 'sm' && 'text-sm',
+        extraClassName,
       )}
     >
       {children}
--- src/containers/JobsContainer/containers/TopJobContainer.tsx
+++ src/containers/JobsContainer/containers/TopJobContainer.tsx
@@ -1,32 +1,34 @@
 import React, { FunctionComponent } from 'react';
 import PeintureImg from 'assets/illu/topjob.svg';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import JobsLayout from 'layouts/JobsLayout/JobsLayout';
 import useMediaQuery from 'hooks/useMediaQuery';
+import Pill from 'components/design-system/Pill';
 
 // TODO remove default and use ids from DB
 export const CardJob: FunctionComponent<{ jobId?: string }> = ({ jobId = 'djs54sdf41dsf213' }) => {
-  const history = useHistory();
-
   return (
-    <button
-      onClick={() => history.push(`/metiers/${jobId}`)}
-      className="bg-lena-lightgray px-5 py-7 rounded-lg focus:ring-0 focus:outline-none text-left"
+    <Link
+      to={`/metiers/${jobId}`}
+      className={`flex flex-col bg-lena-lightgray p-4 rounded-lg focus:ring-0 focus:outline-none text-left space-y-2
+      border border-transparent hover:border-lena-lightgray2`}
     >
-      <h3 style={{ color: '#424242' }} className="font-bold mb-1">
+      <div className="self-end">
+        <Pill size="sm" extraClassName="bg-[rgb(219,247,242)]">
+          DEUG, BTS, DUT
+        </Pill>
+      </div>
+      <h3 style={{ color: '#424242' }} className="font-bold mb-1 text-lg">
         Technicien/ne démonstrateur/trice en matériel agricole
       </h3>
-      <p className="text-lena-black mb-5 text-sm">
+      <p className="text-lena-black mb-5">
         Qu'il représente une ou plusieurs marques, le technicien démonstrateur en matériel agricole...
       </p>
-      <span className="bg-lena-blue-alt-light rounded-full text-sm py-2 px-3">DEUG, BTS, DUT</span>
-    </button>
+    </Link>
   );
 };
 
 const TopJobContainer = () => {
-  const isDesktop = useMediaQuery('md');
-
   return (
     <JobsLayout>
       <div className="container flex flex-col items-center py-8 space-y-8">
@@ -37,24 +39,19 @@ const TopJobContainer = () => {
           </p>
         </div>
       </div>
-      <div
-        className="flex flex-1"
-        style={{ background: `url(${PeintureImg}) no-repeat fixed`, backgroundSize: 'cover' }}
-      >
-        <div className="flex-1 px-4 xl:px-8 py-8 grid lg:grid-cols-2 xl:grid-cols-4 gap-2">
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-          <CardJob />
-        </div>
+      <div className="flex flex-1 px-4 xl:px-8 py-8 grid lg:grid-cols-2 xl:grid-cols-4 gap-2">
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
+        <CardJob />
       </div>
     </JobsLayout>
   );
